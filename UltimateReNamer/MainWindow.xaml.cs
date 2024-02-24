0073b1c725d5b1cdd03ba97a1072990772f2ba01
Berk <berkkarakas98@hotmail.com>
2024-02-25 00:35:17 +0300

    Implement title bar functionality
    - Added event handler for mouse down to enable window dragging.
    - Implemented double-click functionality to toggle between window states (normal and maximized).
    - Added event handlers for minimize, maximize/restore, and close buttons to manage window states and application shutdown.

--- UltimateReNamer/MainWindow.xaml.cs
+++ UltimateReNamer/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UltimateReNamer
 {
@@ -20,5 +12,38 @@ namespace UltimateReNamer
         {
             InitializeComponent();
         }
+
+        #region Title Bar
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+            if (e.ClickCount == 2)
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                else
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
     }
 }
