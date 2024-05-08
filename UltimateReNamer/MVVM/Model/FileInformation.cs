bf41fa3efb6b0da14f7cdcab80aa2bf3f3a2e530
Berk <berkkarakas98@hotmail.com>
2024-05-08 19:16:02 +0300

    Add file information model

---
+++ UltimateReNamer/MVVM/Model/FileInformation.cs
@@ -0,0 +1,15 @@
+﻿namespace UltimateReNamer.MVVM.Model
+{
+    public class FileInformation
+    {
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string FilePath { get; set; }
+        public double FileSize { get; set; }
+        public DateTime FileCreationDate { get; set; }
+        public DateTime FileModifiedDate { get; set; }
+        public DateTime FileAccessDate { get; set; }
+        public DateTime FileLastAccessDate { get; set; }
+        public DateTime FileLastWriteDate { get; set; }
+    }
+}
